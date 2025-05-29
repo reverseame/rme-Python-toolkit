e3bd2cc84c91ef488df736f4a64b2d485a995b10
Hector Toral <hectortoraltrabajos@gmail.com>
2025-05-29 18:44:40 +0200

    feat: api-monitor scrapper python refactor

---
+++ packages/api-monitor-toolkit/src/api_monitor_toolkit/utils/callbacks.py
@@ -0,0 +1,9 @@
+import typer
+from api_monitor_toolkit.utils.logger import configure_logger
+
+def verbose_callback(ctx: typer.Context, param: typer.CallbackParam, value: int):
+    if ctx.resilient_parsing:
+        return
+
+    configure_logger(value)
+    return value
